70555543c15eb71e34de4cecefe89a14335203db
github username <github email address>
2021-03-02 14:43:28 -0500

    commit tree b1881adf6c2e0fed87d47101c1e988a7c18cc889

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,53 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if !logged_in?
+        erb :'users/create_user'
+    else
+        redirect '/tweets'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+        erb :'users/login'
+    else
+        redirect '/tweets'
+    end
+  end
+
+  post '/signup' do
+    if !params.values.any? {|v| v == ""}
+        user = User.create(params)
+        session[:id] = user.id
+        redirect '/tweets'
+    else
+        redirect '/signup'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+        session[:id] = user.id
+        redirect '/tweets'
+    else
+        redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+        session.clear
+        redirect '/login'
+    else
+        redirect '/'
+    end
+  end
+
+  get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+      erb :'show'
+  end
 
 end
